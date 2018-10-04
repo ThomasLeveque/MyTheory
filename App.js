38358a61e3ee6856a6006d0f1531883aafde6227
JawedOuk <jawed.oukili@gmail.com>
2018-10-04 15:52:24 +0200

    authentification

--- App.js
+++ App.js
@@ -1,38 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, Dimensions } from 'react-native';
-
-const WIDTH = Dimensions.get('window').width;
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={[styles.container, styles.test]}>
-        <Image
-          source={{uri: 'https://i.chzbgr.com/full/7345954048/h7E2C65F9/'}}
-          style={{width: 320, height:180}}
-        />
-        <Text>
-          On iOS, a React Native ScrollView uses a native UIScrollView.
-          On Android, it uses a native ScrollView.
-
-          On iOS, a React Native Image uses a native UIImageView.
-          On Android, it uses a native ImageView.
-
-          React Native wraps the fundamental native components, giving you
-          the performance of a native app, plus the clean design of React.
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  test: {
-    justifyContent: 'center'
-  }
-});
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { createSwitchNavigator } from 'react-navigation'
+// import the different screens
+import Loading from './screens/LoadingScreen'
+import SignUp from './screens/SignupScreen'
+import Login from './screens/LoginScreen'
+import Main from './screens/MainScreen'
+// create our app's navigation stack
+const App = createSwitchNavigator(
+    {
+        Loading,
+        SignUp,
+        Login,
+        Main
+    },
+    {
+        initialRouteName: 'Loading'
+    }
+)
+export default App
---
+++ screens/LoadingScreen.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'firebase'
+
+export default class Loading extends React.Component {
+    componentDidMount() {
+
+        let config = {
+            apiKey: "AIzaSyBHZn9I7pannM6aEVR6jG6F2rXHb91Hank",
+            authDomain: "mytheorie-317c9.firebaseapp.com",
+            databaseURL: "https://mytheorie-317c9.firebaseio.com",
+            projectId: "mytheorie-317c9",
+            storageBucket: "mytheorie-317c9.appspot.com",
+            messagingSenderId: "675898618947"
+        };
+
+        firebase.initializeApp(config);
+        firebase.auth().onAuthStateChanged(user => {
+            this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+        })
+
+    }
+
+    render()
+    {
+        return (
+            <View style={styles.container}>
+                <Text>Loading</Text>
+                <ActivityIndicator size="large"/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
