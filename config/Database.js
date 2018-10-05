aec5c4bf2c236afd111b5af9c69edf3b1c85bae5
JawedOuk <jawed.oukili@gmail.com>
2018-10-05 10:37:02 +0200

    Basic add item

---
+++ config/Database.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+let config = {
+    apiKey: "AIzaSyBHZn9I7pannM6aEVR6jG6F2rXHb91Hank",
+    authDomain: "mytheorie-317c9.firebaseapp.com",
+    databaseURL: "https://mytheorie-317c9.firebaseio.com",
+    projectId: "mytheorie-317c9",
+    storageBucket: "mytheorie-317c9.appspot.com",
+    messagingSenderId: "675898618947"
+};
+
+let app = firebase.initializeApp(config)
+export const db = app.database()
+
+export const addItem =  (item) => {
+    db.ref('/items').push({
+        name: item
+    })
+}
