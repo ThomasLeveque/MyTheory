bb046829d6eb21cc0690fe97a9870717039b867f
Thomas Leveque <thomasleveque@MacBook-Pro-de-Thomas.local>
2018-10-11 10:23:56 +0200

    get data of the actual user and show it on the screen

--- screens/main/ProfilScreen.js
+++ screens/main/ProfilScreen.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import firebase from 'firebase'
 
 export default class ProfilScreen extends React.Component {
+    state = {
+        currentUser: null,
+        user: null
+    }
+
+    componentWillMount () {
+        const { currentUser } = firebase.auth()
+        this.setState({ currentUser })
+    }
+
+    componentDidMount () {
+        this.getUserData()
+    }
+
+    getUserData = async () => {
+        const { currentUser } = this.state
+        
+        let database = firebase.database()
+        
+        let user = await database.ref('/users/' + currentUser.uid).once('value')
+        this.setState({ user: user.val() })
+        
+    }
+
     render() {
+        const { user } = this.state
+    
+        if (!user) {
+            return (
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
+        }
+
         return (
             <View style={styles.container}>
-                <Text>
-                    Here come the profile
-                </Text>
+                <Text>{user.name}</Text>
+                <Text>{user.email}</Text>
             </View>
         )
     }
