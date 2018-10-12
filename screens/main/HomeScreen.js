1f9a07a52ddfa16c63b38d91da46b066bc420ac2
Leymonie Valentin <valentin.leymonie@bnpparibas-pf.com>
2018-10-12 17:23:08 +0200

    add home + update package + getTheory

--- screens/main/HomeScreen.js
+++ screens/main/HomeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import firebase from 'firebase'
-
-import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import { List, ListItem, Card, Icon} from "react-native-elements";
+import { ActivityIndicator, FlatList, TouchableOpacity, StyleSheet, Platform, Image, ImageBackground, Text, View, ScrollView, Button } from 'react-native'
 export default class Main extends React.Component {
-    state = { currentUser: null }
+    state = {
+        currentUser: null,
+        data: [],
+        page: 0,
+        loading: false,
+        theories: [],
+    }
 
     signOutUser = async () => {
         try {
@@ -13,30 +19,73 @@ export default class Main extends React.Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount(){
         const { currentUser } = firebase.auth()
         this.setState({ currentUser })
     }
 
+    fetchData = async () => {
+        /*
+        this.setState({ loading: true });
+        const response = await fetch(`https://randomuser.me/api?results=18&seed=hi&page=${this.state.page}`);
+        const json = await response.json();
+        this.setState(state => ({ data: [...state.data, ...json.results], loading: false})) ;
+        */
+        this.setState({ loading: true });
+        const database = firebase.database();
+        const allTheories = await database.ref("/theory").once("value");
+        const theories = Object.values(allTheories.val());
+        this.setState({ theories });
+
+    };
+
+    handleEnd = () => {
+        //console.log('handleEnd called');
+        this.setState(state => ({ page: this.state.page+1 }), () => this.fetchData());
+        this.fetchData();
+    }
+
+    componentDidMount() {
+
+        this.fetchData()
+    }
+
     render() {
-        const { currentUser } = this.state
+        const { currentUser, theories } = this.state
+        if(!theories){ return <ActivityIndicator size='large' animating /> }
+        console.log(theories);
         return (
             <View style={styles.container}>
-                <Text>
-                    Hi {currentUser && currentUser.email}!
-                </Text>
-                <Button
-                    onPress={this.signOutUser}
-                    title="Sign out"
-                />
+
+                <FlatList
+                    data={theories}
+                    onEndReached={() => this.handleEnd()}
+                    onEndReachedThreshold={0}
+                    ListFooterComponent ={() => this.state.loading ? null : <ActivityIndicator size='large' animating /> }
+                    keyExtractor={(x, i) => i}
+                    renderItem={({ item }) =>
+                            <Card
+                            title={item.name}
+                            image={('https://facebook.github.io/react/logo-og.png')}>
+                            <Text style={{marginBottom: 10}}>
+                            The idea with React Native Elements is more about component structure than actual design.
+                            </Text>
+                            <Button
+                            icon={<Icon name='code' color='#ffffff' />}
+                            backgroundColor='#03A9F4'
+                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            title='VIEW NOW' />
+                        </Card>}
+                    />
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        paddingTop:  50,
+        backgroundColor: 'white',
+        height:'110%'
+    },
+
 })
