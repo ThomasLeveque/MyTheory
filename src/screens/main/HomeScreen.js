33651e9f6f4fe487272274352facc4b798676046
Thomas Leveque <thomasleveque@mbp-de-thomas-3.tradelab.local>
2018-11-06 15:12:09 +0100

    HomeScreen with one Subscribe

--- src/screens/main/HomeScreen.js
+++ src/screens/main/HomeScreen.js
@@ -1,67 +1,58 @@
 import React from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 import { Card, Icon, Button } from 'react-native-elements';
-import firebase from 'firebase';
 import { Subscribe } from 'unstated';
 
 import GetTheories from '../../store/GetTheories';
 
-const Main = () => <Subscribe to={[GetTheories]}>{data => <Child data={data} />}</Subscribe>;
+const HomeScreen = () => (
+  <Subscribe to={[GetTheories]}>{data => <Child getTheories={data} />}</Subscribe>
+);
 
 class Child extends React.Component {
   componentDidMount() {
-    this.props.data.fetchData();
+    this.props.getTheories.fetchData();
   }
 
-  signOutUser = async () => {
-    await firebase.auth().signOut();
-  };
-
   render() {
-    return (
-      <Subscribe to={[GetTheories]}>
-        {data => {
-          if (data.state.loading) {
-            return (
-              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Loading</Text>
-                <ActivityIndicator size="large" />
-              </View>
-            );
-          }
+    if (this.props.getTheories.state.loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Loading</Text>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
 
-          return (
-            <View style={styles.container}>
-              <FlatList
-                data={data.state.theories}
-                keyExtractor={({ date }) => date.toString()}
-                renderItem={({ item }) => (
-                  <Card
-                    title={item.name}
-                    image={{
-                      uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
-                    }}
-                  >
-                    <Text style={{ marginBottom: 10 }}>{item.description}</Text>
-                    <Button
-                      backgroundColor="#03A9F4"
-                      onPress={() => {}}
-                      icon={<Icon name="code" color="#ffffff" />}
-                      buttonStyle={{
-                        borderRadius: 0,
-                        marginLeft: 0,
-                        marginRight: 0,
-                        marginBottom: 0,
-                      }}
-                      title="VIEW NOW"
-                    />
-                  </Card>
-                )}
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.getTheories.state.theories}
+          keyExtractor={({ date }) => date.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.name}
+              image={{
+                uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+              }}
+            >
+              <Text style={{ marginBottom: 10 }}>{item.description}</Text>
+              <Button
+                backgroundColor="#03A9F4"
+                onPress={() => {}}
+                icon={<Icon name="code" color="#ffffff" />}
+                buttonStyle={{
+                  borderRadius: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginBottom: 0,
+                }}
+                title="VIEW NOW"
               />
-            </View>
-          );
-        }}
-      </Subscribe>
+            </Card>
+          )}
+        />
+      </View>
     );
   }
 }
@@ -74,4 +65,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default HomeScreen;
