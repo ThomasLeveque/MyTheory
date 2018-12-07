3caf66363cc0cef20270c61bd7942d2a9ad544ef
JawedOuk <jawed.oukili@gmail.com>
2018-12-07 11:32:07 +0100

    Add & get comment on theoryscreen

--- src/screens/main/TheoryScreen.js
+++ src/screens/main/TheoryScreen.js
@@ -1,36 +1,105 @@
-import React from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Image, StyleSheet, Text, View, FlatList } from 'react-native';
+import { Subscribe } from 'unstated';
+import { Card } from 'react-native-elements';
+import InputComponent from '../../components/InputComponent';
+import ButtonComponent from '../../components/ButtonComponent';
 
-const TheoryScreen = props => {
-  const theory = props.navigation.state.params.theory;
-  return (
-    <View style={styles.container}>
-      <Image
-        style={{ width: '100%', height: '50%' }}
-        source={{
-          uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
-        }}
-      />
-      <Text>{theory.name}</Text>
-      <Text>{theory.description}</Text>
-      <View style={styles.containerInline}>
-        <Text style={styles.elementInline}>208 likes</Text>
-        <Button
-          color="red"
-          title="join chat"
-          onPress={() => {}}
-          buttonStyle={{
-            borderRadius: 0,
-            marginLeft: 0,
-            marginRight: 0,
-            marginBottom: 0,
+import Store from '../../store';
+
+const TheoryScreen = props => (
+  <Subscribe to={[Store]}>
+    {store => <Child store={store} navigation={props.navigation} />}
+  </Subscribe>
+);
+
+class Child extends Component {
+  state = {
+    comment: '',
+    comments: [],
+  };
+
+  getComments = async () => {
+    const theory = this.props.navigation.state.params.theory;
+    const comments = await this.props.store.getCommentsTheory(theory.id);
+    this.setState({ comments });
+  };
+
+  componentWillMount = async () => {
+    this.getComments();
+  };
+
+  handleSubmit = async () => {
+    const { comment } = this.state;
+    const theory = this.props.navigation.state.params.theory;
+
+    if (comment.length > 0) {
+      const params = {
+        idTheory: theory.id,
+        idUser: theory.userId,
+        comment,
+      };
+      this.props.store.addCommentTheory(params).then(() => {
+        this.getComments();
+      });
+    }
+  };
+
+  render() {
+    const theory = this.props.navigation.state.params.theory;
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ width: '100%', height: '50%' }}
+          source={{
+            uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
           }}
         />
+        <Text>{theory.name}</Text>
+        <Text>{theory.description}</Text>
+        <View style={styles.containerInline}>
+          <Text style={styles.elementInline}>208 likes</Text>
+          <Button
+            color="red"
+            title="join chat"
+            onPress={() => {}}
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+            }}
+          />
+        </View>
+        <Text>Comments</Text>
+        <InputComponent
+          value={this.state.comment}
+          onChangeValue={comment => this.setState({ comment })}
+          placeholderInput="Your comment"
+          styleInput={styles.itemInput}
+        />
+        <ButtonComponent
+          textButton="Add comment"
+          styleButton={styles.button}
+          styleText={styles.buttonText}
+          onPress={this.handleSubmit}
+          loading={this.state.loading}
+        />
+        <FlatList
+          data={this.state.comments}
+          keyExtractor={({ date }) => date.toString()}
+          renderItem={({ item }) => (
+            <Card>
+              <Text style={{ marginBottom: 10, color: 'black' }}>{item.comment}</Text>
+              <Text>{item.user.name}</Text>
+              <Text>{item.dateFormat}</Text>
+            </Card>
+          )}
+        />
       </View>
-      <Text>Comments</Text>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -40,6 +109,33 @@ const styles = StyleSheet.create({
   containerInline: {
     flex: 1,
     flexDirection: 'row',
+  },
+  itemInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'black',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center',
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
   },
 });
 
