2eb88620efae87354052a3723c2c26d61c60c630
Thomas Leveque <thomasl5895@gmail.com>
2019-02-24 22:44:52 +0100

    Changing initial route Page

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
           listChat: ListChatScreen,
         },
         {
-          initialRouteName: 'theories',
+          initialRouteName: 'home',
         },
       ),
     },
